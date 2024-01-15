7d3b728b65b8f7e8b2adbe00031c28bb4775c0a4
JheynerLobo21 <jheyner21lobo@gmail.com>
2024-01-15 17:54:18 -0500

    subiendo encryptServices

--- src/main/java/com/dialisis/dialisisperitoneal/service/encryption/EncryptionServiceBackend.java
+++ src/main/java/com/dialisis/dialisisperitoneal/service/encryption/EncryptionServiceBackend.java
@@ -1,13 +1,14 @@
 package com.dialisis.dialisisperitoneal.service.encryption;
 
+import com.dialisis.dialisisperitoneal.service.encryption.servicesEncryEntity.*;
 import lombok.Data;
 import org.springframework.stereotype.Service;
 
 @Service
 @Data
 public class EncryptionServiceBackend {
-    private final String clave = "y362_02j=27w/=42";
-    private final String iv="bs72/=a12dx=o=pl";
+    private final String clave = "vsjki526an#%cdsk";
+    private final String iv="d3r56dcdve3$fds-";
     private final EncryptServiceAdmin admin;
     private EncryptionServiceAlergia alergia;
     private EncryptionServiceChequeo chequeo;
@@ -20,9 +21,7 @@ public class EncryptionServiceBackend {
     private final EncryptionServicePacienteAlergia pacienteAlergia;
     private final EncryptionServiceRecambio recambio;
     private final EncryptionServiceRecambioHecho recambioHecho;
-
-
-    public EncryptionServiceBackend(EncryptServiceAdmin admin) {
+    public EncryptionServiceBackend() {
         this.admin = new EncryptServiceAdmin(iv, clave);
         this.alergia = new EncryptionServiceAlergia(iv, clave);
         this.chequeo = new EncryptionServiceChequeo(iv, clave);
@@ -30,11 +29,12 @@ public class EncryptionServiceBackend {
         this.cuidador = new EncryptionServiceCuidador(iv,clave);
         this.cuidadorPaciente = new EncryptionServiceCuidadorPaciente(iv, clave);
         this.formulaMedicamento = new EncryptionServiceFormulaMedicamento(iv, clave);
-        this.paciente = new EncryptionServicePaciente(iv, clave);
         this.medico = new EncryptionServiceMedico(iv, clave);
         this.paciente = new EncryptionServicePaciente(iv, clave);
         this.pacienteAlergia = new EncryptionServicePacienteAlergia(iv, clave);
         this.recambio = new EncryptionServiceRecambio(iv, clave);
         this.recambioHecho = new EncryptionServiceRecambioHecho(iv, clave);
     }
+
+
 }
--- src/main/java/com/dialisis/dialisisperitoneal/service/encryption/EncryptionServiceFrontend.java
+++ src/main/java/com/dialisis/dialisisperitoneal/service/encryption/EncryptionServiceFrontend.java
@@ -1,22 +1,29 @@
 package com.dialisis.dialisisperitoneal.service.encryption;
+
+import com.dialisis.dialisisperitoneal.service.encryption.servicesEncryEntity.*;
+
 import lombok.Data;
 import org.springframework.stereotype.Service;
 
 @Service
 @Data
 public class EncryptionServiceFrontend {
-
-    private final String clave = "vsjki526an#%cdsk";
-    private final String iv="d3r56dcdve3$fds-";
+    private final String clave = "y362_02j=27w/=42";
+    private final String iv="bs72/=a12dx=o=pl";
     private final EncryptServiceAdmin admin;
+    private EncryptionServiceAlergia alergia;
+    private EncryptionServiceChequeo chequeo;
+    private EncryptionServiceCita cita;
+    private EncryptionServiceCuidador cuidador;
+    private EncryptionServiceCuidadorPaciente cuidadorPaciente;
+    private EncryptionServiceFormulaMedicamento formulaMedicamento;
     private final EncryptionServiceMedico medico;
     private final EncryptionServicePaciente paciente;
     private final EncryptionServicePacienteAlergia pacienteAlergia;
     private final EncryptionServiceRecambio recambio;
     private final EncryptionServiceRecambioHecho recambioHecho;
 
-
-    public EncryptionServiceFrontend(EncryptServiceAdmin admin, EncryptionServiceMedico medico, EncryptionServicePaciente paciente, EncryptionServicePacienteAlergia pacienteAlergia, EncryptionServiceRecambio recambio, EncryptionServiceRecambioHecho recambioHecho) {
+    public EncryptionServiceFrontend() {
         this.admin = new EncryptServiceAdmin(iv, clave);
         this.alergia = new EncryptionServiceAlergia(iv, clave);
         this.chequeo = new EncryptionServiceChequeo(iv, clave);
@@ -24,7 +31,6 @@ public class EncryptionServiceFrontend {
         this.cuidador = new EncryptionServiceCuidador(iv,clave);
         this.cuidadorPaciente = new EncryptionServiceCuidadorPaciente(iv, clave);
         this.formulaMedicamento = new EncryptionServiceFormulaMedicamento(iv, clave);
-        this.paciente = new EncryptionServicePaciente(iv, clave);
         this.medico = new EncryptionServiceMedico(iv, clave);
         this.paciente = new EncryptionServicePaciente(iv, clave);
         this.pacienteAlergia = new EncryptionServicePacienteAlergia(iv, clave);
