a58e7948accd68ba8817b40b110ccb8f33981a75
leider <leideryesidmm@gmail.com>
2024-01-15 10:07:45 -0500

    separando controladores por modulos

--- src/main/java/com/dialisis/dialisisperitoneal/context/CorsConfiguration.java
+++ src/main/java/com/dialisis/dialisisperitoneal/context/CorsConfiguration.java
@@ -9,16 +9,10 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-<<<<<<< HEAD
                 //Se pone el primer puerto que haya en el frontend en el docker-compose XD
                 .allowedOrigins("http://localhost:8103")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowedHeaders("Content-Type", "Accept")
-=======
-                .allowedOrigins("http://localhost:8103")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
-                .allowedHeaders("*")
->>>>>>> 75d3c35f43080621a2c9aa4c33d31570ebafed00
                 .allowCredentials(false)
                 .maxAge(3600);
     }
