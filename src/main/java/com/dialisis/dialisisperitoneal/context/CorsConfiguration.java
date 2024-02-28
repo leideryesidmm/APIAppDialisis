b9ee145a3a5d2184e2511ca10e9cc423dbcbdfc2
leider <leideryesidmm@gmail.com>
2024-02-28 14:16:36 -0500

    ultimas correcciones

--- src/main/java/com/dialisis/dialisisperitoneal/context/CorsConfiguration.java
+++ src/main/java/com/dialisis/dialisisperitoneal/context/CorsConfiguration.java
@@ -1,17 +1,19 @@
 package com.dialisis.dialisisperitoneal.context;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://studentsprojects.cloud.ufps.edu.co/misaludrenal/","https://studentsprojects.cloud.ufps.edu.co/misaludrenalpruebas/")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
-                .allowedHeaders("Content-Type", "Accept")
+                .allowedHeaders("*")
                 .allowCredentials(false)
                 .maxAge(3600);
     }
--- src/main/java/com/dialisis/dialisisperitoneal/security/WebSecurityConfig.java
+++ src/main/java/com/dialisis/dialisisperitoneal/security/WebSecurityConfig.java
@@ -6,39 +6,44 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
 @EnableWebSecurity
 @Configuration
-class WebSecurityConfig{
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     JWTAuthorizationFilter jwtAuthorizationFilter;
 
-    @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
 
         http
                 .csrf((csrf) -> csrf.disable())
-                .cors().configurationSource(request -> {
-                    CorsConfiguration corsConfiguration = new CorsConfiguration();
-                    corsConfiguration.applyPermitDefaultValues();
-
-                    // Permitir métodos específicos
-                    corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE"));
-
-                    return corsConfiguration;
-                })
-                .and()
+                .cors().and()
                 .authorizeRequests()
                         .antMatchers("/paciente/findPacienteByCedula/{tipo}","/Usuario/findMedicoByCedula/{tipo}","/Usuario/findAdmin", "/Usuario/claveFrontend", "/Usuario/ivFrontend").permitAll()
                         .anyRequest().authenticated()
                 .and()
                 .addFilterAfter(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
-        return http.build();
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(false);
+        configuration.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
